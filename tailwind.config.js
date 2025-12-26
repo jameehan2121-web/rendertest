2dcf33b332af168bd517ad6c6619fe67470f1476
jameehan2121-web <jameehan2121@gmail.com>
2025-12-25 20:31:18 -0500

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./client/src/**/*.{js,ts,jsx,tsx}",
+    "./index.html"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
